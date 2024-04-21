bd4ad6d4b48027892855b2036ac7e3ea66a3baea
RoboCode9 <ingresshelps@gmail.com>
2024-04-21 12:06:52 -0500

    ingredients page working

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -2,6 +2,7 @@
 
 import { ref, onMounted, computed } from 'vue'
 import useAPI from '@/composables/useAPI';
+import router from '@/router';
 
 const { api } = useAPI()
 
@@ -15,11 +16,32 @@ onMounted(() => {
     })
 })
 
+const searchIngredients = computed(() => {
+    if (!searchIngredients) return ingredients
+
+    return ingredients.value.filter( (i) => i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase()))
+})
+
 </script>
 <template>
     <div class="p-8 pb-0 text-orange-500">
         <h1 class="text-4x1 font-bold mb-4"> Search Meals By ingredients</h1>
     </div>
 
-    {{ ingredients }}
+    <!-- {{ ingredients }} -->
+
+    <div class="p-10">
+        <input type="text" v-model="keyword" class="rounded border-gray-200 w-full bg-white mb-5" placeholder="Enter ingredient name" />
+
+        <div class="grid grid-cols-4 gap-5">
+            <router-link 
+                v-for="ingredient in searchIngredients" 
+                :key="ingredient.idIngredient" 
+                :to="{name: 'MealsByIngredients', params: {ingredient: ingredient.strIngredient}}" 
+                class="bg-white rounded p-3 mb-3 shadow block">
+                    <img :src="`http://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`" />
+                    <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
+            </router-link>
+        </div>
+    </div>
 </template>
